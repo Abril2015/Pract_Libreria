74a4a60eb7da02f577852d4d71f85a3f6ec2f2a0
Abril Zayas <72897553+Abril2015@users.noreply.github.com>
2020-10-21 00:06:26 -0700

    Add files via upload

---
+++ Prueba_Libreria/Prueba_Libreria/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_Libreria
+{
+    public class Mates
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public Mates(double pA, double pB)
+        {
+            a = pA;
+            b = pB;
+            c = 0;
+        }
+         public double C { get { return c; } }
+
+        public double suma()
+        {
+            c = a + b;
+            return c;
+        }
+
+        public double resta()
+        {
+            c = a - b;
+            return c;
+        }
+    }
+}
